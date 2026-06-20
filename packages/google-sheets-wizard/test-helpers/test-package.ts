72b989f915d74e2b4ce76865002e7deb34f9d507
Damian Sire <damiansire@gmail.com>
2026-06-20 15:09:21 -0500

    chore(google-sheets-wizard): habilita flags estrictos de tsconfig (noUnusedLocals/Parameters, exactOptionalPropertyTypes, noUncheckedIndexedAccess, etc) y elimina variable sin uso

--- packages/google-sheets-wizard/test-helpers/test-package.ts
+++ packages/google-sheets-wizard/test-helpers/test-package.ts
@@ -1,7 +1,5 @@
 export const testPackage = (GoogleSheetsWizard: any) => {
   describe("GoogleSheetsWizard", () => {
-    let wizard: any;
-
     beforeAll(() => {
       expect(GoogleSheetsWizard).toBeDefined();
     });
